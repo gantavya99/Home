73ee0ef59a28ec5d8ab4d45ec101817ab89cb72d
Gantavya Saraswat <gantavya99@gmail.com>
2024-03-18 16:43:27 +0530

    Fixed padding

--- Home/src/components/Additional.jsx
+++ Home/src/components/Additional.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Additional = () => {
   return (
     <div>
-    <div className="m-6 h-20 border border-gray-300 border-r-0 border-l-0 lg:mb-0 lg:mt-2">
-      <h2 className="text-[#003339] mt-5 text-2xl font-semibold lg:mt-1 lg:text-sm">
+    <div className="m-6 pb-8 border border-gray-300 border-r-0 border-l-0 lg:mb-0 lg:mt-2 lg:h-20">
+      <h2 className="text-[#003339] mt-5 text-xl font-semibold lg:mt-1 lg:text-sm">
         Additional Info
       </h2>
       <div className="flex justify-between mt-4 lg:mt-1 lg:mb-0 lg:text-xs">
