da5b529edcf01684bb8443257cd97983f1fe0b9e
Gantavya Saraswat <gantavya99@gmail.com>
2024-03-18 14:09:21 +0530

    Added responsive screens

--- Home/src/components/Card.jsx
+++ Home/src/components/Card.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BsClouds } from "react-icons/bs";
+
 const Card = () => {
   return (
-    <div className="bg-[#003339] w-[380px] h-24 mt-12 ml-6 rounded-3xl lg:w-[320px]">
-      <div className="flex justify-around font-bold items-center">
-        {[...Array(5)].map((_, index) => {
-          return (
-            <div key={index} className="text-white mt-4">
-              <div>12:00</div>
-              <div className="ml-2">
-                <BsClouds />
+    <div className="flex justify-center mt-12">
+      <div className="bg-[#003339] w-[380px] h-24 rounded-3xl lg:w-60">
+        <div className="flex justify-around font-bold items-center h-full">
+          {[...Array(5)].map((_, index) => {
+            return (
+              <div key={index} className="text-white mt-4">
+                <div>12:00</div>
+                <div className="ml-2">
+                  <BsClouds />
+                </div>
+                <div className="ml-2">24</div>
               </div>
-              <div className="ml-2">24</div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- Home/src/components/Home.jsx
+++ Home/src/components/Home.jsx
@@ -50,84 +50,81 @@ const Home = () => {
   const formattedDate = `${month} ${day}, ${year}`;
 
   return (
-    <div className="flex justify-center h-screen overflow-visible">
+    <div className="flex justify-center h-screen">
       {!show && (
         <div
-          className="bg-cover bg-no-repeat bg-center w-full md:w-1/3"
+          className="bg-cover bg-no-repeat bg-center w-full lg:w-1/4"
           style={{ backgroundImage: `url(${Img})` }}
         >
           <div className="flex justify-between ">
-            <div onClick={() => setShow(true)} className="m-10">
+            <div onClick={() => setShow(true)} className="m-10 lg:m-6">
               <IoCloseOutline
                 color="white"
-                className="bg-transparent text-4xl cursor-pointer"
+                className="bg-transparent text-4xl cursor-pointer lg:text-2xl"
               />
             </div>
-            <div className="m-12">
-              <button className="bg-[#ff2d55] px-4 py-1 rounded-md text-white border-none text-md">
+            <div className="m-12 lg:m-7">
+              <button className="bg-[#ff2d55] px-4 py-1 rounded-md text-white border-none text-md lg:px-2 lg:py-0">
                 Live
               </button>
             </div>
           </div>
-          <div className="mx-10 mt-10">
-            <div className="flex items-center text-xl text-white bg-gray-800 bg-opacity-20 rounded-xl p-4 py-2 mb-10 w-2/3  bg-opacity-1 cursor-pointer">
+          <div className="mx-8 mt-10 lg:mt-14 lg:mx-6">
+            <div className="flex items-center text-xl text-white bg-gray-800 bg-opacity-20 rounded-xl p-4 py-2 mb-10 w-2/3  bg-opacity-1 cursor-pointer lg:p-2 lg:text-base lg:w-[170px] lg:mb-5">
               <MdLocationOn />
               <button className="ml-2">Current Location</button>
             </div>
 
-            <h1 className="text-white font-semibold text-6xl ">New York,</h1>
-            <h1 className="text-white font-semibold text-6xl">United States</h1>
+            <h1 className="text-white font-semibold text-6xl lg:text-3xl lg:mb-3">New York,</h1>
+            <h1 className="text-white font-semibold text-6xl lg:text-3xl">United States</h1>
           </div>
         </div>
       )}
       {/* //second screen */}
       {show && (
-        <div className="bg-cover bg-no-repeat bg-center h-screen w-full md:w-1/3 bg-white">
-          <div className="flex justify-between ">
-            <div className="m-10" onClick={() => setShow(false)}>
+        <div className="bg-cover bg-no-repeat bg-center h-full w-full md:w-1/4 bg-white">
+          <div className="flex justify-between">
+            <div className="m-10 lg:m-6" onClick={() => setShow(false)}>
               <HiMiniBars3BottomLeft
                 color="black"
-                className="bg-transparent text-4xl cursor-pointer"
+                className="bg-transparent text-4xl cursor-pointer lg:text-2xl"
               />
             </div>
-            <div className="m-10">
+            <div className="m-10 lg:m-6">
               <IoSearchOutline
                 color="black"
-                className="bg-transparent text-4xl cursor-pointer"
+                className="bg-transparent text-4xl cursor-pointer lg:text-2xl"
               />
             </div>
           </div>
-          <div className="ml-5 mt-10 flex">
+          <div className="ml-5 mt-10 flex lg:mt-0 lg:ml-7">
             <div>
-              <h1 className="text-black font-semibold text-3xl">New York,</h1>
-              <h1 className="text-black font-semibold text-3xl">
+              <h1 className="text-black font-semibold text-3xl lg:text-lg">New York,</h1>
+              <h1 className="text-black font-semibold text-3xl lg:text-lg">
                 United States
               </h1>
-              <h2 className="mt-3 font-semibold text-gray-400">
+              <h2 className="mt-3 font-semibold text-gray-400 lg:text-xs lg:mt-1">
                 {formattedDate}
               </h2>
               <div className="flex items-center">
-                <BsClouds color="#057bff" className="mt-3 mr-2" />
-                <h2 className="mt-3 font-semibold text-[#057bff]">Cloudy</h2>
+                <BsClouds color="#057bff" className="mt-3 mr-2 lg:text-sm lg:mt-1" />
+                <h2 className="mt-3 font-semibold text-[#057bff] lg:text-xs lg:mt-1">Cloudy</h2>
               </div>
             </div>
             <div className="flex items-center">
               <img
                 src={ny}
                 alt="New York Image"
-                className="rounded-3xl h-32 w-[160px] ml-14 absolute lg:ml-4 lg:h-28 lg:w-[145px]"
+                className="rounded-3xl h-32 w-[160px] ml-14 absolute lg:ml-4 lg:h-20 lg:w-[120px] lg:rounded-xl"
               />
-              <button className="bg-[#ff2d55] px-4 py-1 left-36 top-10 rounded-md text-white border-none text-md relative lg:left-24 lg:top-8">
+              <button className="bg-[#ff2d55] px-4 py-1 left-36 top-10 rounded-md text-white border-none text-md relative lg:left-24 lg:top-5 lg:text-xs lg:px-2 ">
                 Live
               </button>
             </div>
           </div>
           <Card />
           <Additional />
-        
           <Chart />
-            
-         
         </div>
       )}
     </div>
